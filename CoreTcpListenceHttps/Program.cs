64eafc3489029cb5963d6df6b58fda2f9d448861
bingguang <bingguang@outlook.com>
2018-07-23 10:25:28 +0800

    Initial commit

---
+++ CoreTcpListenceHttps/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net;
+using System.Net.Security;
+using System.Net.Sockets;
+using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Web;
+
+namespace CoreTcpListenceHttps
+{
+    class Program
+    {
+        private static string InstallCertificate(byte[] bytes)
+        {
+            X509Certificate2 certificate = new X509Certificate2(bytes, "123");
+            X509Store store = new X509Store(StoreName.Root, StoreLocation.LocalMachine);
+            store.Open(OpenFlags.ReadWrite);
+            store.Add(certificate);
+            store.Close();
+            return certificate.GetSerialNumberString();
+        }
+
+        public static string ReadOnce(SslStream sslStream, int size = 1024)
+        {
+            byte[] buffer = new byte[size];
+            var bytes = sslStream.Read(buffer, 0, buffer.Length);
+            Decoder decoder = Encoding.UTF8.GetDecoder();
+            var chars = new char[decoder.GetCharCount(buffer, 0, bytes)];
+            decoder.GetChars(buffer, 0, bytes, chars, 0);
+            return new string(chars);
+        }
+
+        public static void RequestWork(TcpListener obj)
+        {
+            TcpClient client = obj.AcceptTcpClient();
+            var networkStream = client.GetStream();
+            Console.WriteLine("有连接接入...");
+            while (networkStream.CanRead)
+            {
+                using (var sslStream = new SslStream(networkStream))
+                {
+                    #region 写入证书
+                    try
+                    {
+                        var test = InstallCertificate(Properties.Resources.myCert);
+                        X509Store store = new X509Store(StoreName.Root);
+                        store.Open(OpenFlags.IncludeArchived);
+                        X509Certificate2 cert =
+                            store.Certificates.Find(X509FindType.FindBySerialNumber, test, false)[0];
+                        sslStream.AuthenticateAsServer(cert, false, SslProtocols.Tls, true);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex);
+                        Console.ReadLine();
+                        continue;
+                    }
+                    #endregion
+
+                    var strHead = ReadOnce(sslStream) + ReadOnce(sslStream);
+                    if (string.IsNullOrWhiteSpace(strHead))
+                    {
+                        // todo:证书不对
+                    }
+                    var strHeadList = strHead.Split(new[] { "\r\n" }, StringSplitOptions.None);
+                    string body = "";
+                    foreach (var s in strHeadList)
+                    {
+                        int length = "Content-Length:".Length;
+                        if (s.IndexOf("Content-Length:") > -1)
+                        {
+                            if (int.Parse(s.Substring(length, s.Length - length).Trim()) > 0)
+                            {
+                                //todo:处理 body
+                                body = ReadOnce(sslStream) + ReadOnce(sslStream, 1024000);
+                                Console.WriteLine("rawBody:" + body);
+                            }
+                        }
+                    }
+                    Dictionary<string, string> dic = new Dictionary<string, string>();
+                    var rawUrlData = strHeadList[0].Split(' ')[1].TrimStart('?', '/');
+                    NameValueCollection rawUrlDataCollection = HttpUtility.ParseQueryString(rawUrlData);
+                    Console.WriteLine("url参数:" + rawUrlData);
+                    foreach (string key in rawUrlDataCollection.Keys)
+                    {
+                        if (!string.IsNullOrWhiteSpace(key))
+                        {
+                            dic.Add(key, rawUrlDataCollection[key]);
+                        }
+                    }
+                    string callbackStr = "";
+                    if (dic.Keys.Contains("callback") && !string.IsNullOrWhiteSpace(dic["callback"]))
+                    {
+                        callbackStr = dic["callback"];
+                    }
+
+                    string statusLine = "HTTP/1.1 200 OK\r\n";
+                    byte[] responsestatusLineBytes = Encoding.UTF8.GetBytes(statusLine);
+
+                    //?callback = jQuery110205463076986739899_1532054092048 &
+                    string ResultStr = "123";
+                    if (!string.IsNullOrWhiteSpace(callbackStr))
+                    {
+                        ResultStr = string.Format("{0}({1})", callbackStr, ResultStr);
+                    }
+                    byte[] responseBodyBytes = Encoding.UTF8.GetBytes(ResultStr);
+                    string responseHead = string.Format(
+                        "Content-Type:text/html;charset=UTF-8\r\nContent-Length:{0}\r\n", ResultStr.Length);
+                    byte[] responseHeadBytes = Encoding.UTF8.GetBytes(responseHead);
+                    sslStream.Write(responsestatusLineBytes, 0, responsestatusLineBytes.Length);
+                    sslStream.Write(responseHeadBytes, 0, responseHeadBytes.Length);
+                    sslStream.Write(new byte[] { 13, 10 }, 0, 2);
+                    sslStream.Write(responseBodyBytes, 0, responseBodyBytes.Length);
+                    networkStream.Flush();
+                }
+            }
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            TcpListener listener = new TcpListener(IPAddress.Any, 1024);
+            listener.Start();
+            while (true)
+            {
+                Console.WriteLine("等待客户端的连接...");
+                RequestWork(listener);
+            }
+
+        }
+    }
+}
---
+++ HttpWebRequestDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace HttpWebRequestDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("请输入要发送的数据...");
+                var str = Console.ReadLine();
+                SendHttps(str);
+            }
+        }
+
+        public static void SendHttps(string str)
+        {
+            byte[] data = Encoding.UTF8.GetBytes(str);
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create("https://localhost:1024/");
+            req.Method = "POST";
+            using (Stream Requeststream = req.GetRequestStream())
+            {
+                Requeststream.Write(data, 0, data.Length);
+                Requeststream.Close();
+
+                Console.WriteLine("发送完成");
+                HttpWebResponse httpWebResponse = (HttpWebResponse)req.GetResponse();
+                Console.WriteLine("等待返回数据...");
+                using (Stream responseStream = httpWebResponse.GetResponseStream())
+                {
+                    StreamReader streamReader = new StreamReader(responseStream, Encoding.UTF8);
+                    Console.WriteLine(streamReader.ReadToEnd());
+                    Console.WriteLine("数据已收到");
+                }
+            }
+        }
+    }
+}
